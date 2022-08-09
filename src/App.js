e62abd59c62476c0c8c8ce2534b1cc6d445974a8
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-08-09 09:56:37 +0800

    configured tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="open">
+        <h1 className="open-title-1">open-title-1</h1>
+        <h2 className="open-title-2">open-title-2</h2>
+        <h3 className="open-title-3">open-title-3</h3>
+        <h4 className="open-title-4">open-title-4</h4>
+        <h1 className="open-heading-1">open-heading-1</h1>
+        <h2 className="open-heading-2">open-heading-2</h2>
+        <h3 className="open-heading-3">open-heading-3</h3>
+        <h4 className="open-heading-4">open-heading-4</h4>
+        <h5 className="open-heading-5">open-heading-5</h5>
+        <h6 className="open-heading-6">open-heading-6</h6>
+        <p className="open-paragraph">open-paragraph</p>
+        <p className="open-paragraph-sm">open-paragraph-sm</p>
+        <p className="open-button">open-button</p>
+      </div>
+      <div className="poppins">
+        <h1 className="poppins-title-1">poppins-title-1</h1>
+        <h2 className="poppins-title-2">poppins-title-2</h2>
+        <h3 className="poppins-title-3">poppins-title-3</h3>
+        <h4 className="poppins-title-4">poppins-title-4</h4>
+        <h1 className="poppins-heading-1">poppins-heading-1</h1>
+        <h2 className="poppins-heading-2">poppins-heading-2</h2>
+        <h3 className="poppins-heading-3">poppins-heading-3</h3>
+        <h4 className="poppins-heading-4">poppins-heading-4</h4>
+        <h5 className="poppins-heading-5">poppins-heading-5</h5>
+        <h6 className="poppins-heading-6">poppins-heading-6</h6>
+        <p className="poppins-paragraph">poppins-paragraph</p>
+        <p className="poppins-paragraph-sm">poppins-paragraph-sm</p>
+        <p className="poppins-button">poppins-button</p>
+      </div>
     </div>
   );
 }
