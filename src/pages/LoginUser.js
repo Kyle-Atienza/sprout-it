777bdc1603027cc23a25d786f5472736c2425bb4
Kyle-Atienza <atienzakylefrancis@gmail.com>
2022-10-11 23:29:34 +0800

    password toggle

--- src/pages/LoginUser.js
+++ src/pages/LoginUser.js
@@ -89,6 +89,7 @@ export const LoginUser = () => {
               id="password"
               placeholder="Password"
               onChange={onChange}
+              password
             />
             <a
               className="text-right open-button text-light-700"
