5ed73e332f427512ed2205ad39e131b7225630c7
Felix Jerome Dela Fuente <dlfuente.felixjerome@gmail.com>
2022-10-16 14:56:43 +0800

    fix: merge conflict

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -28,52 +28,55 @@ export function Settings() {
 
   return (
     <>
-      <div className="flex flex-row w-screen min-h-screen">
-        <div className="w-0 lg:w-1/6">
+      <div className='flex flex-row w-screen min-h-screen'>
+        <div className='w-0 lg:w-1/6'>
           <SideNavBar />
         </div>
 
-        <div className="flex flex-col w-full lg:w-5/6">
-          <div className="w-full">
-            <TopNavBar pageName="Settings" />
+        <div className='flex flex-col w-full lg:w-5/6'>
+          <div className='w-full'>
+            <TopNavBar pageName='Settings' />
           </div>
-          <div className="w-full flex gap-10 my-4 py-4 px-4 md:px-6 lg:px-9">
-            <section className="w-full lg:w-1/2 ">
+          <div className='w-full flex gap-10 my-4 py-4 px-4 md:px-6 lg:px-9'>
+            <section className='w-full lg:w-1/2 '>
               <InviteForm />
             </section>
-            <section className="w-full lg:w-1/2 mx-10 my-6 shadow-md bg-light-100 rounded-xl">
-              <table className="w-full text-sm text-left">
-                <thead className=" poppins-paragraph text-secondary-300">
-                  <tr>
-                    <th scope="col" className="py-4 px-6">
-                      Name
-                    </th>
-                    <th scope="col" className="py-4 px-6">
-                      Email
-                    </th>
-                    <th scope="col" className="py-4 px-6"></th>
-                  </tr>
-                </thead>
-                <tbody className="open-paragraph">
-                  {users.map((user) => {
-                    return (
-                      <tr
-                        key={user._id}
-                        className="bg-light-100 hover:bg-light-200 border-b dark:bg-gray-800 dark:border-gray-700 transition-all duration-300 ease-in-out cursor-pointer"
-                      >
-                        <td className="py-4 px-6">{user.name}</td>
-                        <td className="py-4 px-6">{user.email}</td>
-                        <td className="py-4 px-6 ">
-                          <button onClick={() => onDeleteUser(user._id)}>
-                            <DeleteFilled className="text-red-500" />
-                          </button>
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </section>
+
+            <table className='w-full text-sm text-left lg:w-1/2 mx-10 my-6 shadow-md bg-light-100 rounded-xl'>
+              <thead className=' poppins-paragraph text-secondary-300'>
+                <tr>
+                  <th scope='col' className='py-4 px-6'>
+                    Name
+                  </th>
+                  <th scope='col' className='py-4 px-6'>
+                    Email
+                  </th>
+                  <th scope='col' className='py-4 px-6'>
+                    Role
+                  </th>
+                  <th scope='col' className='py-4 px-6'></th>
+                </tr>
+              </thead>
+              <tbody className='open-paragraph'>
+                {users.map((user) => {
+                  return (
+                    <tr
+                      key={user._id}
+                      className='bg-light-100 hover:bg-light-200 border-b dark:bg-gray-800 dark:border-gray-700 transition-all duration-300 ease-in-out cursor-pointer'
+                    >
+                      <td className='py-4 px-6'>{user.name}</td>
+                      <td className='py-4 px-6'>{user.email}</td>
+                      <td className='py-4 px-6'>{user.role}</td>
+                      <td className='py-4 px-6 '>
+                        <button onClick={() => onDeleteUser(user._id)}>
+                          <DeleteFilled className='text-red-500' />
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>  
+            </table>
           </div>
         </div>
       </div>
